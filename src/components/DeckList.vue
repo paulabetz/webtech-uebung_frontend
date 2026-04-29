f624a26219c82e87fc1654a4dda810466319ce4a
jonas <s0599425@htw-berlin.de>
2026-04-29 13:20:53 +0200

    DeckList component mit v-for (M2)

---
+++ src/components/DeckList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Meine Decks</h2>
+    <ul>
+      <li v-for="deck in decks" :key="deck.id">
+        <strong>{{ deck.name }}</strong>
+        – {{ deck.cards.length }} Karten
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const decks = [
+  { id: 1, name: "Web-Tech Grundlagen", cards: [{}, {}] },
+  { id: 2, name: "Java Basics",         cards: [{}] },
+  { id: 3, name: "Datenbanken",         cards: [{}, {}, {}] },
+]
+</script>
