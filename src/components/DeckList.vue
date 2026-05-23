79035097ed0b274e248692001fb3568b91f8d03d
jonas <s0599425@htw-berlin.de>
2026-05-23 11:29:20 +0200

    env für localhost und production angelegt

--- src/components/DeckList.vue
+++ src/components/DeckList.vue
@@ -11,9 +11,18 @@
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
 const decks = [
   { id: 1, name: "Web-Tech Grundlagen", cards: [{}, {}] },
   { id: 2, name: "Java Basics",         cards: [{}] },
   { id: 3, name: "Datenbanken",         cards: [{}, {}, {}] },
 ]
+
+onMounted(async () => {
+  const baseUrl = import.meta.env.VITE_BACKEND_BASE_URL
+  const response = await fetch(`${baseUrl}/api/decks`)
+  decks.value = await response.json()
+})
+
 </script>
